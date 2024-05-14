409500b337ad4d0b06a1a86e0a71adf0b06b756f
Lucas Souza <lucassouzafranco1@gmail.com>
2024-05-14 15:36:41 -0300

    ✨ feat(background): add background image and container to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import CourseGrid from './components/CourseGrid/CourseGrid';
-import Menu from './components/Menu/Menu';
+import CourseGrid from "./components/CourseGrid/CourseGrid";
+import Menu from "./components/Menu/Menu";
+import Background from "./assets/background.jpg";
+import "./App.css";
 
 function App() {
-
   return (
-    <> 
+    <div id="appContainer"> {/* Adicione uma div para envolver todo o conteúdo */}
       <Menu />
-      <CourseGrid /> 
-    </>
+      <CourseGrid />
+      <img src={Background} alt="background" className="backgroundImage" />
+    </div>
   );
 }
 
